df97e9b842330054623825820497565ce5cf2075
skyGwork <skyGworkk@gmail.com>
2022-08-16 18:02:57 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './css/config/var.css';
-import './css/config/reset.css';
-import './css/config/util.css';
-import './css/app.css';
+
+import './css/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
