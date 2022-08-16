ce32c85b1d1ca2ffddf690a63fbf80dbe6ee6c73
skyGwork <skyGworkk@gmail.com>
2022-08-16 20:33:10 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import ExpenseItem from './components/ExpenseItem';
+// one- default
+// import ExpenseItem from './components/ExpenseItem';
+
+//two- named
+// import { ExpenseItem } from './components/ExpenseItem';
+
+// function App() {
+//   return (
+//     <div>
+//       <h2>Let's get started!</h2>
+//       <ExpenseItem />
+//     </div>
+//   );
+// }
+
+//three named +default
+
+import { ExpenseItem,Test } from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem />
+      <Test />
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,3 +1,33 @@
-export default function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+// one
+// export default function ExpenseItem() {
+//   return <h2>Expense item!  </h2>;
+// }
+
+// named
+// function ExpenseItem() {
+//   return <h2>Expense item! </h2>;
+// }
+
+// export { ExpenseItem };
+
+// THREE
+
+// function ExpenseItem() {
+//   return <h2>Expense item! </h2>;
+// }
+// export default function Test() {
+//   return <p>Its test</p>
+// };
+
+// export { ExpenseItem };
+
+// FOUR
+function ExpenseItem() {
+  return <h2>Expense item! </h2>;
 }
+
+function Test() {
+  return <p>Its test</p>
+};
+
+export { ExpenseItem, Test };
